f08cacd5a14587f6dc7d8f120b45a8e1e0c8fec4
Rafael Poveda <raerpo@gmail.com>
2017-11-11 20:27:07 -0300

    litter fix

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,6 @@
 const buildHistoMinuteUrl = (fromCurrency = 'USD', toCurrency = 'BTC', rangeOfExtraction = 60, toTimestamp) => {
   const toTimestampFixed = toTimestamp || new Date().getTime(); 
-  return `https://min-api.cryptocompare.com/data/histominute?fsym=${fromCurrency}&tsym=${toCurrency}&limit=${rangeOfExtraction}&aggregate=1&toTs=${toTimestampFixed}&extraParams=wallmart-test`;
+  return `https://min-api.cryptocompare.com/data/histominute?fsym=${fromCurrency}&tsym=${toCurrency}&limit=${rangeOfExtraction}&aggregate=1&toTs=${toTimestampFixed}&extraParams=walmart-test`;
 }
 
 module.exports = {
